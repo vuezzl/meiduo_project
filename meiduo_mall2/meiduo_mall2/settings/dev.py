085a4e1735a26514a0439e563125a2329e815d5b
詹志莲 <2585583870@qq.com>
2021-01-24 20:46:55 +0800

    ３配置mysql 数据库

--- meiduo_mall2/meiduo_mall2/settings/dev.py
+++ meiduo_mall2/meiduo_mall2/settings/dev.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'meiduo_mall2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql', # 数据库引擎
+        'HOST': '127.0.0.1', # 数据库主机
+        'PORT': 3306, # 数据库端口
+        'USER': 'itcast', # 数据库用户名
+        'PASSWORD': '123456', # 数据库用户密码
+        'NAME': 'meiduo_mall' # 数据库名字
+    },
 }
+
 
 
 # Password validation
