b2542ac17bbe7a8c47a7ba4a67a9f02640435206
詹志莲 <2585583870@qq.com>
2021-01-26 13:05:54 +0800

    10　用户名重复

--- meiduo_mall2/apps/users/urls.py
+++ meiduo_mall2/apps/users/urls.py
@@ -6,4 +6,6 @@ from django.urls import path
 urlpatterns = [
     # 用户名重复
     path('usernames/<username:username>/count/', views.UsernameCountView.as_view()),
+    # 手机号重复
+    path('mobiles/<mobile:mobile>/count/', views.MobileCountView.as_view()),
 ]
--- meiduo_mall2/apps/users/views.py
+++ meiduo_mall2/apps/users/views.py
@@ -24,3 +24,20 @@ class UsernameCountView(View):
         }
 
         return JsonResponse(data_dict)
+
+
+# 手机号重复
+
+class MobileCountView(View):
+    def get(self,request,mobile):
+        count = User.objects.filter(mobile=mobile).count()
+        data_dict = {
+            'code': 0,
+            'errmsg': 'ok',
+            'count': count
+
+        }
+
+        return JsonResponse(data_dict)
+
+
